7e1cbb9659cabef166be8fd152c74b63e352689d
tiiedye <tiiedye@gmail.com>
2021-12-15 08:50:21 -0800

    writing code to generate letters

--- main.py
+++ main.py
@@ -5,3 +5,11 @@
 
 with open("./Input/Names/invited_names.txt") as guest_names:
     guests = guest_names.readlines()
+
+for guest in guests:
+    guest_name = guest.replace("\n", "")
+    with open("./Input/Letters/starting_letter.txt") as letter:
+        letter_text = letter.read()
+        new_letter_text = letter_text.replace("[name]", f"{guest_name}")
+    with open(f"./Output/ReadyToSend/letter_to_{guest_name}.txt", mode="w") as new_letter:
+        new_letter.write(new_letter_text)
